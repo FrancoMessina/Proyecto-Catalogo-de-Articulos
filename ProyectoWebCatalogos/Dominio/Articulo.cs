4dbff9706b0644720c1177954b5676ae1fd26e5b
Franco Messina <franco.m2017@outlook.com>
2022-08-01 19:08:49 -0300

    agregando arquitectura en capas

---
+++ ProyectoWebCatalogos/Dominio/Articulo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public enum EMarcas { Samsung, Apple, Sony, Huawei, Motorola }
+    public enum ECategorias { Celulares,Televisor,Medias,Audio}
+    public class Articulo
+    {
+        private int id;
+        private string codigo;
+        private string nombre;
+        private string descripcion;
+        private Marca marca;
+        private Categoria categoria;
+        private string urlImagen;
+        private decimal precio;
+
+        public Articulo()
+        {
+
+        }
+        public Articulo(string codigo, string nombre, string descripcion, string urlImagen)
+        {
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.urlImagen = urlImagen;
+        }
+        public int Id { get => id; set => id = value; }
+        [DisplayName("Código")]
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        [DisplayName("Descripción")]
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public Marca Marca { get => marca; set => marca = value; }
+        [DisplayName("Categoría")]
+        public Categoria Categoria { get => categoria; set => categoria = value; }
+        public string UrlImagen { get => urlImagen; set => urlImagen = value; }
+        public decimal Precio { get => precio; set => precio = value; }
+    }
+}
---
+++ ProyectoWebCatalogos/Negocio/ValidacionesArticulo/ValidarArticulo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Negocio.Excepciones;
+namespace Negocio.ValidacionesArticulo
+{
+    public static class ValidarArticulo
+    {
+        public static bool ValidarCamposArticulo(string codigo, string nombre, string descripcion, string urlImagen, string precio)
+        {
+            if (string.IsNullOrWhiteSpace(codigo) || string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(descripcion)
+                || string.IsNullOrWhiteSpace(urlImagen) || string.IsNullOrWhiteSpace(precio))
+            {
+                throw new CamposVaciosONullException("Cargar todos los datos del articulo!");
+            }
+            return true;
+        }
+        public static bool ValidarCampo(string dato)
+        {
+            if (dato.Length > 40 || dato.Length < 2)
+            {
+                throw new CampoInvalidoException("El campo tiene que tener más de dos caracteres y menos de 40");
+            }
+            return true;
+        }
+        public static bool ValidarImagen(string url)
+        {
+            if (url.Length < 5)
+            {
+                throw new CampoInvalidoException("El campo tiene que tener más de 5 caracteres");
+            }
+            return true;
+        }
+        public static bool ValidarPrecio(string precio)
+        {
+            if (!decimal.TryParse(precio, out decimal auxPrecio))
+            {
+                throw new PrecioInvalidoException("El Precio tiene que ser númerico!");
+            }
+            else if (auxPrecio <  0)
+            {
+                throw new PrecioInvalidoException("El Precio no puede ser negativo");
+            }
+            return true;
+        }
+        public static bool ValidarTexto(string dato)
+        {
+            if (!VerificarSoloLetras(dato))
+            {
+                throw new CampoInvalidoException("Ingresar solo letras");
+            }
+            return true;
+        }
+        private static bool VerificarSoloLetras(string dato)
+        {
+            foreach (char letra in dato)
+            {
+                if (!char.IsLetter(letra))
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
